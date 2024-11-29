f45218e7cc7830d2cc6219218600288b60209d1a
Ashraful05 <jamalrasu420@gmail.com>
2024-11-29 21:04:12 +0600

    top_banner_component_part_1_completed_with_banner_image

---
+++ src/components/TopBanner/TopBanner.jsx
@@ -0,0 +1,15 @@
+import React, {Component, Fragment} from "react";
+import {Col, Container, Row} from "react-bootstrap";
+import '../../asset/css/custom.css'
+
+export default class TopBanner extends Component{
+    render() {
+        return(
+            <Fragment>
+                <Container className={'topFixedBanner'}>
+
+                </Container>
+            </Fragment>
+        )
+    }
+}
