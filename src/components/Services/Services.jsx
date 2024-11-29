1ad773d1e21731612411fd79b065b95b17db67d6
Ashraful05 <jamalrasu420@gmail.com>
2024-11-29 22:45:09 +0600

    services_component_part_2_completed_with_service_card_name_description

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,6 +3,8 @@ import {Container,Row,Col} from "react-bootstrap";
 import designIcon from '../../asset/image/design.png';
 import ecommerceIcon from '../../asset/image/ecommerce.png';
 import webIcon from '../../asset/image/web.png';
+import '../../asset/css/custom.css';
+import '../../asset/css/bootstrap.min.css';
 
 export default class Services extends Component{
     render() {
@@ -11,18 +13,25 @@ export default class Services extends Component{
                 <Container>
                     <Row>
                         <Col lg={4} md={6} sm={12}>
-                            <div>
+                            <div className={'serviceCard text-center'}>
                                 <img className={'ecommerceIcon'} src={ecommerceIcon} alt=""/>
+                                <h2 className={'serviceName'}>Ecommerce</h2>
+                                <p className={'serviceDescription'}>I will design and develop ecommerce online store website.</p>
                             </div>
                         </Col>
                         <Col lg={4} md={6} sm={12}>
-                            <div>
+                            <div className={'serviceCard text-center'}>
                                 <img className={'designIcon'} src={designIcon} alt=""/>
+                                <h2 className={'serviceName'}>WebDesign</h2>
+                                <p className={'serviceDescription'}>Qualified web design and attractive effects which catches visitors eyes.</p>
                             </div>
                         </Col>
                         <Col lg={4} md={6} sm={12}>
-                            <div>
+                            <div className={'serviceCard text-center'}>
                                 <img className={'webIcon'} src={webIcon} alt=""/>
+                                <h2 className={'serviceName'}>Web Development</h2>
+                                <p className={'serviceDescription'}>Clean and fresh code to make website perform dynamically</p>
+
                             </div>
                         </Col>
                     </Row>
