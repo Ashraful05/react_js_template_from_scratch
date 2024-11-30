34b115201a882069f2850fa08b44e2533b84935c
Ashraful05 <jamalrasu420@gmail.com>
2024-11-30 21:16:36 +0600

    navigation_component_module_part_2_completed_with_nav_title_color_change_on_scroll

--- src/components/TopNavigation/TopNavigation.jsx
+++ src/components/TopNavigation/TopNavigation.jsx
@@ -2,12 +2,28 @@ import React, {Component, Fragment} from "react";
 import {Container, Nav, Navbar, NavDropdown} from "react-bootstrap";
 
 export default class TopNavigation extends Component{
+
+    constructor() {
+        super();
+        this.state={navBarTitle:'navTitle'}
+    }
+    onScroll=()=>{
+        if(window.scrollY>100){
+            this.setState({navBarTitle: 'navTitleScroll'})
+        }else if(window.scrollY<100){
+            this.setState({navBarTitle:'navTitleScroll'})
+        }
+    }
+    componentDidMount() {
+        window.addEventListener('scroll',this.onScroll);
+    }
+
     render() {
         return(
             <Fragment>
                 <Navbar collapseOnSelect fixed={'top'} expand="lg" className="bg-body-tertiary">
                     <Container>
-                        <Navbar.Brand href="#home">React App From Scratch</Navbar.Brand>
+                        <Navbar.Brand href="#home" className={this.state.navBarTitle}>React App From Scratch</Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
                         <Navbar.Collapse id="responsive-navbar-nav">
                             <Nav className="me-auto">
