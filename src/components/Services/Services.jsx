3338c8e01bfed93ad3635b84c0c52cb7c0a5a860
Ashraful05 <jamalrasu420@gmail.com>
2025-02-13 21:44:57 +0600

    services_api_consuming_completed_with_showing_data_using_loop_with_map_function

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -3,38 +3,42 @@ import {Container,Row,Col} from "react-bootstrap";
 import designIcon from '../../asset/image/design.png';
 import ecommerceIcon from '../../asset/image/ecommerce.png';
 import webIcon from '../../asset/image/web.png';
+import RestClient from "../../RestApi/RestClient";
+import AppUrl from "../../RestApi/AppUrl";
 
 
 export default class Services extends Component{
+
+    constructor() {
+        super();
+        this.state={
+            myData:[],
+        }
+    }
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.Services).then(result=>{
+            this.setState({myData:result});
+        });
+    }
+
     render() {
+        const myList = this.state.myData;
+        const myView = myList.map(myList=>{
+            return <Col lg={4} md={6} sm={12}>
+                <div className={'serviceCard text-center'}>
+                    <img className={'ecommerceIcon'} src={myList.service_logo} alt=""/>
+                    <h2 className={'serviceName'}>{myList.service_name}</h2>
+                    <p className={'serviceDescription'}>{myList.service_description}.</p>
+                </div>
+            </Col>
+        })
         return(
             <Fragment>
                 <Container className={'text-center'}>
                     <h1 className={'serviceMainTitle'}>My Services</h1>
                     <div className={'bottom'}></div>
                     <Row>
-                        <Col lg={4} md={6} sm={12}>
-                            <div className={'serviceCard text-center'}>
-                                <img className={'ecommerceIcon'} src={ecommerceIcon} alt=""/>
-                                <h2 className={'serviceName'}>Ecommerce</h2>
-                                <p className={'serviceDescription'}>I will design and develop ecommerce online store website.</p>
-                            </div>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <div className={'serviceCard text-center'}>
-                                <img className={'designIcon'} src={designIcon} alt=""/>
-                                <h2 className={'serviceName'}>WebDesign</h2>
-                                <p className={'serviceDescription'}>Qualified web design and attractive effects which catches visitors eyes.</p>
-                            </div>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <div className={'serviceCard text-center'}>
-                                <img className={'webIcon'} src={webIcon} alt=""/>
-                                <h2 className={'serviceName'}>Web Development</h2>
-                                <p className={'serviceDescription'}>Clean and fresh code to make website perform dynamically</p>
-
-                            </div>
-                        </Col>
+                        { myView }
                     </Row>
 
                 </Container>
