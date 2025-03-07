8629b54cb853f82c69d151a8cf12073d32ca65e7
ashrafulmd389 <ashrafulmd389@gmail.com>
2025-03-07 12:00:40 +0600

    all_course_project_page_api_consuming_completed

--- src/components/AllCourses/AllCourses.jsx
+++ src/components/AllCourses/AllCourses.jsx
@@ -2,72 +2,48 @@ import React from "react";
 import {Component, Fragment} from "react";
 import {Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import RestClient from "../../RestApi/RestClient";
+import AppUrl from "../../RestApi/AppUrl";
 
 export default class AllCourses extends Component{
+    constructor() {
+        super();
+        this.state={
+            myData:[]
+        }
+    }
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.AllCourse).then(result=>{
+            this.setState({myData:result})
+        })
+    }
+
     render() {
+
+        const myList = this.state.myData;
+        const myView = myList.map(myList=>{
+            return <Col lg={6} md={12} sm={12}>
+                <Row>
+                    <Col lg={6} md={6} sm={12} className={'p-2'}>
+                        <img className={'courseImage'} src={myList.small_image} alt=""/>
+                    </Col>
+                    <Col lg={6} md={6} sm={12} >
+                        <h4 className={'text-center serviceName'}>{myList.short_title}</h4>
+                        <p className={'text-center serviceDescription'}>{myList.short_description}.</p>
+                        <Link className='courseViewMore float-lg-none'to={'/coursedetails'}>View Details</Link>
+                    </Col>
+                </Row>
+
+            </Col>
+        })
+
         return(
             <Fragment>
                 <Container className={'text-center'}>
                     <h1 className={'serviceMainTitle'}>My Courses</h1>
                     <div className={'bottom'}></div>
                     <Row>
-                        <Col lg={6} md={12} sm={12}>
-                            <Row>
-                                <Col lg={6} md={6} sm={12} className={'p-2'}>
-                                    <img className={'courseImage'} src="https://img.freepik.com/free-psd/e-learning-banner-design-template_23-2149113592.jpg?ga=GA1.1.236297427.1733904633&semt=ais_hybrid" alt=""/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} >
-                                    <h4 className={'text-center serviceName'}>PHP</h4>
-                                    <p className={'text-center serviceDescription'}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link className='courseViewMore float-lg-none' to={'/coursedetails'}>View Details</Link>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} className={'p-2'}>
-                                    <img className={'courseImage'} src="https://img.freepik.com/free-vector/laptop-technology-with-cap-graduation-digital-document_24877-53136.jpg?ga=GA1.1.236297427.1733904633&semt=ais_hybrid" alt=""/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12}>
-                                    <h4 className={'text-center serviceName'}>Laravel</h4>
-                                    <p className={'text-center serviceDescription'}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link className='courseViewMore float-lg-none' to={'/coursedetails'}>View Details</Link>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} className={'p-2'}>
-                                    <img className={'courseImage'} src="https://img.freepik.com/free-vector/e-learning-icons-flat_1284-3950.jpg?ga=GA1.1.236297427.1733904633&semt=ais_hybrid" alt=""/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12}>
-                                    <h4 className={'text-center serviceName'}>Laravel</h4>
-                                    <p className={'text-center serviceDescription'}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link className='courseViewMore float-lg-none' to={'/coursedetails'}>View Details</Link>
-                                </Col>
-                            </Row>
-
-                        </Col>
-                        <Col lg={6} md={12} sm={12}>
-                            <Row>
-                                <Col lg={6} md={6} sm={12} className={'p-2'}>
-                                    <img className={'courseImage'} src="https://img.freepik.com/free-vector/e-learning-icons-flat_1284-3950.jpg?ga=GA1.1.236297427.1733904633&semt=ais_hybrid" alt=""/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} >
-                                    <h4 className={'text-center serviceName'}>JavaScript</h4>
-                                    <p className={'text-center serviceDescription'}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link className='courseViewMore float-lg-none' to={'/coursedetails'}>View Details</Link>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} className={'p-2'}>
-                                    <img className={'courseImage'} src="https://img.freepik.com/premium-vector/elearning-icon-3d-illustration-from-elearning-collection-creative-elearning-3d-icon-web-design-templates-infographics-more_676904-770.jpg?ga=GA1.1.236297427.1733904633&semt=ais_hybrid" alt=""/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} >
-                                    <h4 className={'text-center serviceName'}>React JS</h4>
-                                    <p className={'text-center serviceDescription'}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link className='courseViewMore float-lg-none' to={'/coursedetails'}>View Details</Link>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} className={'p-2'}>
-                                    <img className={'courseImage'} src="https://img.freepik.com/free-vector/laptop-technology-with-cap-graduation-digital-document_24877-53136.jpg?ga=GA1.1.236297427.1733904633&semt=ais_hybrid" alt=""/>
-                                </Col>
-                                <Col lg={6} md={6} sm={12} >
-                                    <h4 className={'text-center serviceName'}>React JS</h4>
-                                    <p className={'text-center serviceDescription'}>Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                    <Link className='courseViewMore float-lg-none' to={'/coursedetails'}>View Details</Link>
-                                </Col>
-                            </Row>
-                        </Col>
+                        { myView }
                     </Row>
                 </Container>
             </Fragment>
--- src/components/AllProjects/AllProject.jsx
+++ src/components/AllProjects/AllProject.jsx
@@ -1,96 +1,47 @@
 import React, {Component, Fragment} from "react";
 import {Button, Card, Col, Container, Row} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import RestClient from "../../RestApi/RestClient";
+import AppUrl from "../../RestApi/AppUrl";
 
 export default class AllProject extends Component{
+    constructor() {
+        super();
+        this.state={
+            myData:[],
+        }
+    }
+
+    componentDidMount() {
+        RestClient.GetRequest(AppUrl.AllProjects).then(result=>{
+            this.setState({myData:result});
+        })
+    }
     render() {
+        const myList = this.state.myData;
+        const myView = myList.map(myList=>{
+            return <Col lg={4} md={6} sm={12}>
+                <Card className={'projectCard'}>
+                    <Card.Img variant="top" src={myList.image_one} />
+                    <Card.Body>
+                        <Card.Title className={'serviceName'}>{myList.project_name}</Card.Title>
+                        <Card.Text className={'serviceDescription'}>
+                            {myList.project_description}.
+                        </Card.Text>
+                        <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
+                    </Card.Body>
+                </Card>
+            </Col>
+        })
         return(
             <Fragment>
                 <Container className={'text-center'}>
                     <h1 className={'serviceMainTitle'}>Recent Projects</h1>
                     <div className={'bottom'}></div>
                     <Row>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className={'projectCard'}>
-                                <Card.Img variant="top" src="https://img.freepik.com/free-vector/laptop-technology-with-book-statistics-bar_24877-53125.jpg?t=st=1733905369~exp=1733908969~hmac=6b3bc840a1388455044c94e7a455cde1db417afa4b64e200fae6fd7241fc3388&w=740" />
-                                <Card.Body>
-                                    <Card.Title className={'serviceName'}>Project Name One</Card.Title>
-                                    <Card.Text className={'serviceDescription'}>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className={'projectCard'}>
-                                <Card.Img variant="top" src="https://img.freepik.com/premium-photo/2d-vector-illustration-graduation-success-college-study-training-courses-school-educational_918459-25772.jpg?semt=ais_hybrid" />
-                                <Card.Body>
-                                    <Card.Title className={'serviceName'}>Project Name Two</Card.Title>
-                                    <Card.Text className={'serviceDescription'}>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className={'projectCard'}>
-                                <Card.Img variant="top" src="https://img.freepik.com/premium-photo/2d-vector-illustration-graduation-success-college-study-training-courses-school-educational_918459-25775.jpg?semt=ais_hybrid" />
-                                <Card.Body>
-                                    <Card.Title className={'serviceName'}>Project Name Three</Card.Title>
-                                    <Card.Text className={'serviceDescription'}>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
+                        { myView }
                     </Row>
-                    <Row>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className={'projectCard'}>
-                                <Card.Img variant="top" src="https://img.freepik.com/free-vector/laptop-technology-with-book-statistics-bar_24877-53125.jpg?t=st=1733905369~exp=1733908969~hmac=6b3bc840a1388455044c94e7a455cde1db417afa4b64e200fae6fd7241fc3388&w=740" />
-                                <Card.Body>
-                                    <Card.Title className={'serviceName'}>Project Name One</Card.Title>
-                                    <Card.Text className={'serviceDescription'}>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className={'projectCard'}>
-                                <Card.Img variant="top" src="https://img.freepik.com/premium-photo/2d-vector-illustration-graduation-success-college-study-training-courses-school-educational_918459-25772.jpg?semt=ais_hybrid" />
-                                <Card.Body>
-                                    <Card.Title className={'serviceName'}>Project Name Two</Card.Title>
-                                    <Card.Text className={'serviceDescription'}>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                    <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                        <Col lg={4} md={6} sm={12}>
-                            <Card className={'projectCard'}>
-                                <Card.Img variant="top" src="https://img.freepik.com/premium-photo/2d-vector-illustration-graduation-success-college-study-training-courses-school-educational_918459-25775.jpg?semt=ais_hybrid" />
-                                <Card.Body>
-                                    <Card.Title className={'serviceName'}>Project Name Three</Card.Title>
-                                    <Card.Text className={'serviceDescription'}>
-                                        Some quick example text to build on the card title and make up the
-                                        bulk of the card's content.
-                                    </Card.Text>
-                                   <Button variant="primary"><Link className={'linkStyle'} to={'/projectdetails'}>View More</Link></Button>
-                                </Card.Body>
-                            </Card>
-                        </Col>
-                    </Row>
+
                 </Container>
             </Fragment>
         )
